0f84a730c6abf29a847b3cea675dfd08e29cc13a
smhrd <smhrd@DESKTOP-J5BS04K>
2023-04-19 17:34:21 +0900

    image entity 수정

--- src/main/java/com/togetherTeam/platform/entity/Image.java
+++ src/main/java/com/togetherTeam/platform/entity/Image.java
@@ -2,11 +2,16 @@ package com.togetherTeam.platform.entity;
 
 import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
 public class Image {
 
-	private int pro_no;
-	private int img_no;
-	private String img_url;
-	private LocalDateTime img_date;
+	private int pro_no; // 물품번호
+	private int img_no; // 이미지 번호
+	private LocalDateTime img_date; // 이미지 등록일
+	private String upload_path; // 이미지 경로
+	private String uuid; // 이미지 이름
+	private String file_name; // 파일경로
 	
 }
